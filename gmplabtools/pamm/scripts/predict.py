6090c7df58b3dc138c20f966057414d91a06fc34
Giovanni <9051577+giovannidoni@users.noreply.github.com>
2020-04-17 16:23:09 +0100

    Use label for rates by defaults

--- gmplabtools/pamm/scripts/predict.py
+++ gmplabtools/pamm/scripts/predict.py
@@ -21,7 +21,7 @@ def main(config):
         save = np.hstack((x_, clusters))
         np.savetxt(f[:-4] + "_result.txt", save)
 
-        rates = ClusterRates(40, "proba").calculate_matrix(clusters)
+        rates = ClusterRates(40, "label").calculate_matrix(save[:, -1])
         np.savetxt(f[:-4] + "_rates.txt", rates)
 
 
