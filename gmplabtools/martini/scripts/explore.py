a3336bcde6f2f7018afd504cbb258d3080af6851
Giovanni <9051577+giovannidoni@users.noreply.github.com>
2020-06-09 21:02:04 +0100

    Re-establish all main

--- gmplabtools/martini/scripts/explore.py
+++ gmplabtools/martini/scripts/explore.py
@@ -5,10 +5,9 @@ from .parameters import *
 from .simulation import SetupSim
 
 
-def main(config):
-
-    for simulation in SetupSim(config):
-        simulation.setup()
+def main(config, n=1):
+    for _ in range(n):
+        simulation = SetupSim(config).setup()
         simulation.prepare()
         simulation.run()
 
@@ -18,6 +17,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", dest="config", type=str,
                         help="config file")
+    parser.add_argument("-n", dest="n", type=int,
+                        help="number of sim")
     args = parser.parse_args()
 
-    main(get_config(args.config))
+    main(get_config(args.config), args.n)
