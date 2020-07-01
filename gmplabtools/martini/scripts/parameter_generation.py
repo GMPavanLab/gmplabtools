afceda78df99078df943b39b6e515fc41f7fa90b
giovannidoni <9051577+giovannidoni@users.noreply.github.com>
2020-07-01 14:43:04 +0100

    Add skopt to iterate over parameter generation (#12)
    * Add skopt to iterate over parameter generation
    
    * Restrict parameter range
    
    * Use mean strategy

---
+++ gmplabtools/martini/scripts/parameter_generation.py
@@ -0,0 +1,43 @@
+import argparse
+
+import numpy as np
+from skopt import Optimizer
+from skopt.space import Real
+
+from gmplabtools.shared.config import get_config
+from gmplabtools.martini.parameters import Param
+
+
+def main(config, n, res):
+    dim = len(config.params['fields'])
+    parameters = Param(config.params['fields'])
+
+    if res is not None:
+        data = np.loadtxt(res)
+        opt = Optimizer([Real(0.03, 0.25), Real(0.03, 0.25)],
+                        base_estimator="gp",
+                        acq_optimizer="auto")
+
+        y = list(data[:, :dim])
+        x = [list(row[:dim]) for row in data]
+        opt.tell(x, y)
+        parameter_list = opt.ask(n, strategy='cl_mean')
+    else:
+        parameter_list = []
+        for i in range(n):
+            parameter_list.append([value for field, value in parameters()])
+
+    np.savetxt("sampled_parameters.txt", np.array(parameter_list))
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", dest="config", type=str,
+                        help="config file")
+    parser.add_argument("-n", dest="n", type=int,
+                        help="sample size")
+    parser.add_argument("-r", dest="result", type=str, default=None,
+                        help="partial result")
+    args = parser.parse_args()
+    main(get_config(args.config), args.n, args.result)
